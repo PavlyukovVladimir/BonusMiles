41e86cfd5df2a9bbaa0b423b5f2ffee031495583
Vladimir <vladimir.pavlyukov@thrive.io>
2022-08-29 19:02:54 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int costOfBonusMile = 20;
+        int costOfTicket = 1254;
+        int bonusMiles;
+
+        bonusMiles = costOfTicket / costOfBonusMile;
+
+        System.out.println(bonusMiles + " bonus miles will be accrued for a ticket worth " + costOfTicket + " ₽");
+    }
+
+}
