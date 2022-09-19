99ec22b0342944246538e36e1aea0aae42e97d30
Vladimir Pavlyukov <yahusim@mail.ru>
2022-09-19 23:19:41 +0300

    change for Домашнее задание к занятию "Testability, авто-тесты, введение в ООП: объекты и методы" task 1

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int calculate(int cost){
+        int costOfBonusMile = 20;
+        int bonusMiles;
+
+        return cost / costOfBonusMile;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,26 @@ public class Main {
 
     public static void main(String[] args) {
         int costOfBonusMile = 20;
-        int costOfTicket = 1254;
+        int costOfTicket = 10_000;
         int bonusMiles;
 
         bonusMiles = costOfTicket / costOfBonusMile;
 
         System.out.println(bonusMiles + " bonus miles will be accrued for a ticket worth " + costOfTicket + " ₽");
+
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        System.out.println("При покупке билета на сумму: " + price);
+        int miles = service.calculate(price);
+        System.out.println("Расчет бонусных миль дает: " + miles);
+
+        String prefix = "Было: " + bonusMiles + " стало: " + miles + ". Результаты вычислений ";
+        String suffix = "совпадают" + "\u001B[0m";
+        if(bonusMiles == miles){
+            System.out.println("\u001B[32m" + prefix + suffix);
+        }else{
+            System.out.println("\u001B[31m" + prefix + "не " + suffix);
+        }
     }
 
 }
